4b54605ed3a1311f76800d2bf2f405266108d453
Josh Goundry <joshuagoundry@gmail.com>
2023-09-05 18:14:10 +0100

    Added files required for implementing GetModuleHandle, GetProcAddress

--- compile.bat
+++ compile.bat
@@ -1,5 +1,4 @@
 @ECHO OFF
 
-rc resources.rc
-cvtres /MACHINE:x64 /OUT:resources.o resources.res
-cl.exe /nologo /Ox /MT /W0 /GS- /DNDEBUG /Tcimplant.cpp /link /OUT:implant.exe /SUBSYSTEM:WINDOWS /MACHINE:x64 resources.o
+cl.exe /nologo /Ox /MT /W0 /GS- /DNDEBUG /Tp *.cpp /link /OUT:implant.exe /SUBSYSTEM:WINDOWS
+del *.obj
